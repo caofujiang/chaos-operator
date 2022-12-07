eaee11391e96a5b78ab6acb65b597130506052d0
caofujiang <caofu_jiang@126.com>
2022-12-07 22:54:25 +0800

    context-experiment

--- pkg/client/clientset/versioned/typed/litmuschaos/v1alpha1/chaosexperiment.go
+++ pkg/client/clientset/versioned/typed/litmuschaos/v1alpha1/chaosexperiment.go
@@ -20,6 +20,7 @@ package v1alpha1
 
 import (
 	"time"
+	"context"
 
 	v1alpha1 "github.com/litmuschaos/chaos-operator/pkg/apis/litmuschaos/v1alpha1"
 	scheme "github.com/litmuschaos/chaos-operator/pkg/client/clientset/versioned/scheme"
@@ -37,15 +38,15 @@ type ChaosExperimentsGetter interface {
 
 // ChaosExperimentInterface has methods to work with ChaosExperiment resources.
 type ChaosExperimentInterface interface {
-	Create(*v1alpha1.ChaosExperiment) (*v1alpha1.ChaosExperiment, error)
-	Update(*v1alpha1.ChaosExperiment) (*v1alpha1.ChaosExperiment, error)
-	UpdateStatus(*v1alpha1.ChaosExperiment) (*v1alpha1.ChaosExperiment, error)
-	Delete(name string, options *v1.DeleteOptions) error
-	DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error
-	Get(name string, options v1.GetOptions) (*v1alpha1.ChaosExperiment, error)
-	List(opts v1.ListOptions) (*v1alpha1.ChaosExperimentList, error)
-	Watch(opts v1.ListOptions) (watch.Interface, error)
-	Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ChaosExperiment, err error)
+	Create(ctx context.Context, chaosExperiment *v1alpha1.ChaosExperiment) (*v1alpha1.ChaosExperiment, error)
+	Update(ctx context.Context, chaosExperiment *v1alpha1.ChaosExperiment) (*v1alpha1.ChaosExperiment, error)
+	UpdateStatus(ctx context.Context,chaosExperiment *v1alpha1.ChaosExperiment) (*v1alpha1.ChaosExperiment, error)
+	Delete(ctx context.Context,name string, options *v1.DeleteOptions) error
+	DeleteCollection(ctx context.Context,options *v1.DeleteOptions, listOptions v1.ListOptions) error
+	Get(ctx context.Context,name string, options v1.GetOptions) (*v1alpha1.ChaosExperiment, error)
+	List(ctx context.Context,opts v1.ListOptions) (*v1alpha1.ChaosExperimentList, error)
+	Watch(ctx context.Context,opts v1.ListOptions) (watch.Interface, error)
+	Patch(ctx context.Context,name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ChaosExperiment, err error)
 	ChaosExperimentExpansion
 }
 
@@ -64,20 +65,20 @@ func newChaosExperiments(c *LitmuschaosV1alpha1Client, namespace string) *chaosE
 }
 
 // Get takes name of the chaosExperiment, and returns the corresponding chaosExperiment object, and an error if there is any.
-func (c *chaosExperiments) Get(name string, options v1.GetOptions) (result *v1alpha1.ChaosExperiment, err error) {
+func (c *chaosExperiments) Get(ctx context.Context,name string, options v1.GetOptions) (result *v1alpha1.ChaosExperiment, err error) {
 	result = &v1alpha1.ChaosExperiment{}
 	err = c.client.Get().
 		Namespace(c.ns).
 		Resource("chaosexperiments").
 		Name(name).
 		VersionedParams(&options, scheme.ParameterCodec).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // List takes label and field selectors, and returns the list of ChaosExperiments that match those selectors.
-func (c *chaosExperiments) List(opts v1.ListOptions) (result *v1alpha1.ChaosExperimentList, err error) {
+func (c *chaosExperiments) List(ctx context.Context,opts v1.ListOptions) (result *v1alpha1.ChaosExperimentList, err error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
@@ -88,47 +89,48 @@ func (c *chaosExperiments) List(opts v1.ListOptions) (result *v1alpha1.ChaosExpe
 		Resource("chaosexperiments").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Watch returns a watch.Interface that watches the requested chaosExperiments.
-func (c *chaosExperiments) Watch(opts v1.ListOptions) (watch.Interface, error) {
+func (c *chaosExperiments) Watch(ctx context.Context,opts v1.ListOptions) (result watch.Interface, err error) {
 	var timeout time.Duration
 	if opts.TimeoutSeconds != nil {
 		timeout = time.Duration(*opts.TimeoutSeconds) * time.Second
 	}
 	opts.Watch = true
-	return c.client.Get().
+	result,err= c.client.Get().
 		Namespace(c.ns).
 		Resource("chaosexperiments").
 		VersionedParams(&opts, scheme.ParameterCodec).
 		Timeout(timeout).
-		Watch()
+		Watch(ctx)
+	return
 }
 
 // Create takes the representation of a chaosExperiment and creates it.  Returns the server's representation of the chaosExperiment, and an error, if there is any.
-func (c *chaosExperiments) Create(chaosExperiment *v1alpha1.ChaosExperiment) (result *v1alpha1.ChaosExperiment, err error) {
+func (c *chaosExperiments) Create(ctx context.Context,chaosExperiment *v1alpha1.ChaosExperiment) (result *v1alpha1.ChaosExperiment, err error) {
 	result = &v1alpha1.ChaosExperiment{}
 	err = c.client.Post().
 		Namespace(c.ns).
 		Resource("chaosexperiments").
 		Body(chaosExperiment).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Update takes the representation of a chaosExperiment and updates it. Returns the server's representation of the chaosExperiment, and an error, if there is any.
-func (c *chaosExperiments) Update(chaosExperiment *v1alpha1.ChaosExperiment) (result *v1alpha1.ChaosExperiment, err error) {
+func (c *chaosExperiments) Update(ctx context.Context,chaosExperiment *v1alpha1.ChaosExperiment) (result *v1alpha1.ChaosExperiment, err error) {
 	result = &v1alpha1.ChaosExperiment{}
 	err = c.client.Put().
 		Namespace(c.ns).
 		Resource("chaosexperiments").
 		Name(chaosExperiment.Name).
 		Body(chaosExperiment).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
@@ -136,7 +138,7 @@ func (c *chaosExperiments) Update(chaosExperiment *v1alpha1.ChaosExperiment) (re
 // UpdateStatus was generated because the type contains a Status member.
 // Add a +genclient:noStatus comment above the type to avoid generating UpdateStatus().
 
-func (c *chaosExperiments) UpdateStatus(chaosExperiment *v1alpha1.ChaosExperiment) (result *v1alpha1.ChaosExperiment, err error) {
+func (c *chaosExperiments) UpdateStatus(ctx context.Context,chaosExperiment *v1alpha1.ChaosExperiment) (result *v1alpha1.ChaosExperiment, err error) {
 	result = &v1alpha1.ChaosExperiment{}
 	err = c.client.Put().
 		Namespace(c.ns).
@@ -144,24 +146,24 @@ func (c *chaosExperiments) UpdateStatus(chaosExperiment *v1alpha1.ChaosExperimen
 		Name(chaosExperiment.Name).
 		SubResource("status").
 		Body(chaosExperiment).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
 
 // Delete takes name of the chaosExperiment and deletes it. Returns an error if one occurs.
-func (c *chaosExperiments) Delete(name string, options *v1.DeleteOptions) error {
+func (c *chaosExperiments) Delete(ctx context.Context,name string, options *v1.DeleteOptions) error {
 	return c.client.Delete().
 		Namespace(c.ns).
 		Resource("chaosexperiments").
 		Name(name).
 		Body(options).
-		Do().
+		Do(ctx).
 		Error()
 }
 
 // DeleteCollection deletes a collection of objects.
-func (c *chaosExperiments) DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error {
+func (c *chaosExperiments) DeleteCollection(ctx context.Context,options *v1.DeleteOptions, listOptions v1.ListOptions) error {
 	var timeout time.Duration
 	if listOptions.TimeoutSeconds != nil {
 		timeout = time.Duration(*listOptions.TimeoutSeconds) * time.Second
@@ -177,7 +179,7 @@ func (c *chaosExperiments) DeleteCollection(options *v1.DeleteOptions, listOptio
 }
 
 // Patch applies the patch and returns the patched chaosExperiment.
-func (c *chaosExperiments) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ChaosExperiment, err error) {
+func (c *chaosExperiments) Patch(ctx context.Context,name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.ChaosExperiment, err error) {
 	result = &v1alpha1.ChaosExperiment{}
 	err = c.client.Patch(pt).
 		Namespace(c.ns).
@@ -185,7 +187,7 @@ func (c *chaosExperiments) Patch(name string, pt types.PatchType, data []byte, s
 		SubResource(subresources...).
 		Name(name).
 		Body(data).
-		Do().
+		Do(ctx).
 		Into(result)
 	return
 }
